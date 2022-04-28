3473b632a4f4454beafe71a153df84e328e6719a
Murat Ülker <muratulker93@gmail.com>
2022-04-28 05:46:05 +0300

    Blog Detail / Section -1

---
+++ app/js/blogDetail.js
@@ -0,0 +1,28 @@
+console.log("Load script.js");
+
+// Instantiating the global app object
+var app = {};
+
+//* Toggle Nav Bar 
+
+const menu = document.querySelector(".menu");
+const menuLink = document.querySelector(".nav-bar-burger");
+const menuOverlay = document.querySelector(".menu-overlay");
+const burger1 = document.querySelector(".nav-bar-burger-inc-1");
+const burger2 = document.querySelector(".nav-bar-burger-inc-2");
+const burger3 = document.querySelector(".nav-bar-burger-inc-3");
+
+menuLink.addEventListener("click", () => {
+  menuOverlay.classList.toggle("open");
+  burger1.classList.toggle('nav-bar-burger-inc');
+  burger2.classList.toggle('nav-bar-burger-inc');
+  burger3.classList.toggle('nav-bar-burger-inc');
+});
+
+//* Search Input - Other Pages
+const searchIconBlack = document.querySelector(".search__icon-black");
+const searchInputBlack = document.querySelector(".search-input-black");
+
+searchIconBlack.addEventListener("click", () => {
+  searchInputBlack.classList.toggle("fade");
+})
