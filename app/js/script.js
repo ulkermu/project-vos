c8a9adf0d263de06d59c4ff71156cae7b6dde64d
Murat Ülker <muratulker93@gmail.com>
2022-05-08 16:02:40 +0300

    Script

--- app/js/script.js
+++ app/js/script.js
@@ -3,9 +3,10 @@ console.log("Load script.js");
 // Instantiating the global app object
 var app = {};
 
-//* Toggle Nav Bar 
+//* Toggle Nav Bar
 
 const menu = document.querySelector(".menu");
+const mobilMenu = document.querySelector(".mobil-menu");
 const menuLink = document.querySelector(".nav-bar-burger");
 const menuOverlay = document.querySelector(".menu-overlay");
 const burger1 = document.querySelector(".nav-bar-burger-inc-1");
@@ -14,24 +15,105 @@ const burger3 = document.querySelector(".nav-bar-burger-inc-3");
 
 menuLink.addEventListener("click", () => {
   menuOverlay.classList.toggle("open");
-  burger1.classList.toggle('nav-bar-burger-inc');
-  burger2.classList.toggle('nav-bar-burger-inc');
-  burger3.classList.toggle('nav-bar-burger-inc');
+  burger1.classList.toggle("nav-bar-burger-inc");
+  burger2.classList.toggle("nav-bar-burger-inc");
+  burger3.classList.toggle("nav-bar-burger-inc");
 });
 
 //* Search Input - Home Page
 
-const searchIcon = document.querySelector(".search__icon");
+const searchIcon = document.querySelector(".search-icon");
 const searchInput = document.querySelector(".search-input");
 
 searchIcon.addEventListener("click", () => {
   searchInput.classList.toggle("fade");
 });
 
-//* Search Input - Other Pages
-const searchIconBlack = document.querySelector(".search__icon-black");
-const searchInputBlack = document.querySelector(".search-input-black");
+//* Swiper
 
-searchIconBlack.addEventListener("click", () => {
-  searchInputBlack.classList.toggle("fade");
-})
+const swiper = new Swiper(".swiper-container", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  initialSlide: 1,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 194,
+    depth: 219.61,
+    modifier: 2,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    320: {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      initialSlide: 1,
+      speed: 300,
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 94,
+        depth: 19.61,
+        modifier: 2,
+        slideShadows: true,
+      },
+    },
+    // When window width is >= 760px
+    760: {
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 194,
+        depth: 219.61,
+        modifier: 2,
+        slideShadows: true,
+      },
+    },
+  },
+});
+
+//* Advanced Price Choose
+
+const circles = document.querySelectorAll(".pricing-circle");
+const pricingByHours = document.getElementById("pricingByHours");
+circles.forEach((circle) => {
+  const index = [...circle.parentElement.children].indexOf(circle);
+  circle.addEventListener("click", () => {
+    const greenLine = document.querySelector(".pricing-inline");
+    greenLine.style.width = `${(index * 100) / (circles.length - 1)}%`;
+    pricingByHours.textContent = `${circle.getAttribute("data-price")}`;
+  });
+});
+
+//* Pricing FAQ
+$(document).ready(function () {
+  $(".pricingFaq-slidedown-headline-mark").click(function () {
+    $(this)
+      .parent()
+      .parent()
+      .find(".pricingFaq-slidedown-hidden")
+      .stop()
+      .slideToggle("normal");
+    $(this).parent().find(".pricingFaq-slidedown-headline-mark").toggle();
+    $(this).parent().find("p").toggleClass("selected-p");
+  });
+
+  $(".pricing-circle").click(function () {
+    if ($(this).hasClass("pricing-circle-bg")) {
+      return;
+    } else {
+      $(".pricing-circle").removeClass("pricing-circle-bg");
+      $(this).addClass("pricing-circle-bg");
+    }
+  });
+});
